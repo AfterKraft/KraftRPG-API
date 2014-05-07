f540853f5e60f667e694afe08972b869471313f9
riking <rikingcoding@gmail.com>
2014-05-07 15:02:28 -0700

    Fix up javadocs for StorageBackend and Active

--- src/main/java/com/afterkraft/kraftrpg/api/storage/StorageBackend.java
+++ src/main/java/com/afterkraft/kraftrpg/api/storage/StorageBackend.java
@@ -20,7 +20,11 @@ import java.util.UUID;
 
 /**
  * A backend for storage of player data. All methods in this class may be
- * assumed to take a long time, and should be called on threads.
+ * assumed to take a long time, and will be called off of the main thread.
+ * (Therefore, no Bukkit API except the Scheduler is safe to access.)
+ *
+ * The PlayerData objects you are provided in savePlayer() will be snapshots,
+ * not the live data objects.
  */
 public interface StorageBackend {
 
@@ -28,14 +32,24 @@ public interface StorageBackend {
      * Attempt to initialize this StorageBackend. If this method does not
      * return true, or throws an exception, the plugin will be disabled.
      *
-     * @return true if no errors
+     * (Main thread)
+     *
+     * @throws anything if an fatal problem occurs - this will halt the plugin
      */
-    public boolean initialize() throws Throwable;
+    public void initialize() throws Throwable;
 
+    /**
+     * Stop any long-running or maintenance tasks, cleanly terminate
+     * connections.
+     *
+     * (Main thread)
+     */
     public void shutdown();
 
     /**
      * Delete the data for the given player.
+     *
+     * Assume different thread.
      *
      * @param uuid UUID of player to delete
      * @return true if player existed
@@ -45,14 +59,20 @@ public interface StorageBackend {
     /**
      * Write the data for the given player.
      *
-     * @param data
-     * @return
+     * Assume different thread.
+     *
+     * @param uuid The player's UUID
+     * @param data A PlayerData snapshot object.
+     * @return unused, just return true for now
      */
     public boolean savePlayer(UUID uuid, PlayerData data);
 
     /**
      * Load the data for the player with the given UUID. If shouldCreate is
      * true, this may not return null.
+     *
+     * Assume different thread. (The implementation does not do this yet,
+     * however, so try to keep the time down, please.)
      *
      * @param uuid UUID of player to load
      * @param shouldCreate whether to allocate space for the player if not
@@ -64,7 +84,9 @@ public interface StorageBackend {
     /**
      * Return a list of every UUID with data.
      *
-     * @return
+     * Assume different thread.
+     *
+     * @return every UUID stored
      */
     public List<UUID> getAllStoredUsers();
 }
